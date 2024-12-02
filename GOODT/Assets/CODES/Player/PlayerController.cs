85f2fefb8eeebe0685b7c652974d9dddf4b1e50c
Sen Yuan B. Samson <115355129+SyGwasTaken@users.noreply.github.com>
2024-12-02 19:43:24 +0800

    hh

--- GOODT/Assets/CODES/Player/PlayerController.cs
+++ GOODT/Assets/CODES/Player/PlayerController.cs
@@ -20,7 +20,9 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            
+            // Remove diagonal movement
+            if (input.x != 0) input.y = 0;
+
             if (input != Vector2.zero)
             {
                 animator.SetFloat("moveX", input.x);
