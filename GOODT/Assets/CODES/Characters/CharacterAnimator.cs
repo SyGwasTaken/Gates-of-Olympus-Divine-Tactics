1a6f20f0de9941e7d02d482471bee75aae5c9990
Sen Yuan B. Samson <115355129+SyGwasTaken@users.noreply.github.com>
2024-12-05 16:40:10 +0800

    psuh

---
+++ GOODT/Assets/CODES/Characters/CharacterAnimator.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+
+    [SerializeField] List<Sprite> walkDownSprites;
+    [SerializeField] List<Sprite> walkUpSprites;
+    [SerializeField] List<Sprite> walkRightSprites;
+    [SerializeField] List<Sprite> walkLeftSprites;  
+    
+    // PARAMETERS
+    public float MoveX {get; set; } 
+    public float MoveY {get; set; } 
+    public bool isMoving {get; set; } 
+     
+    // STATES
+    SpriteAnimator walkDownAnim;
+    SpriteAnimator walkUpAnim;
+    SpriteAnimator walkRightAnim;
+    SpriteAnimator walkLeftAnim;
+
+    SpriteAnimator currentAnim;
+
+    // REFERENCE
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        walkDownAnim = new SpriteAnimator(walkDownSprites, spriteRenderer);
+        walkUpAnim = new SpriteAnimator(walkUpSprites, spriteRenderer);
+        walkRightAnim = new SpriteAnimator(walkRightSprites, spriteRenderer);
+        walkLeftAnim = new SpriteAnimator(walkLeftSprites, spriteRenderer);
+
+        currentAnim = walkDownAnim;
+    }
+
+    private void Update()
+    {
+        if (MoveX == 1)
+            currentAnim = walkRightAnim;
+        else if (MoveX == -1)
+            currentAnim = walkLeftAnim;
+        else if (MoveY == 1)
+            currentAnim = walkUpAnim;
+        else if (MoveY == -1)
+            currentAnim = walkDownAnim;
+            
+    }
+}
+
