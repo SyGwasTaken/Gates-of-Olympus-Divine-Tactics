6a9db15af6c7cfe212b4834aec33e39910ec29e5
Sen Yuan B. Samson <115355129+SyGwasTaken@users.noreply.github.com>
2024-12-05 16:40:22 +0800

    Update NPCController.cs

--- GOODT/Assets/CODES/Characters/NPCController.cs
+++ GOODT/Assets/CODES/Characters/NPCController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class NPCController : MonoBehaviour, Interactables
 {
     [SerializeField] Dialog dialog;
+    
     public void Interact()
     {
         StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
